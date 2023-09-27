2685f54486b44d47827b302efefcdf92978ed0de
baophan17 <baop99720@gmail.com>
2023-09-27 14:03:31 +0700

    Design Responsive form create new user using redux

---
+++ src/store/actions/adminActions.js
@@ -0,0 +1,30 @@
+import actionTypes from './actionTypes';
+import { getAllCodeService } from "../../services/userSevice";
+
+// export const fetchGenderStart = () => ({
+//     type: actionTypes.FETCH_GENDER_START
+// })
+export const fetchGenderStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllCodeService('GENDER');
+            if (res && res.errCode === 0) {
+                dispatch(fetchGenderSuccsess(res.data));
+            } else {
+                dispatch(fetchGenderFailed());
+            }
+        } catch (e) {
+            dispatch(fetchGenderFailed());
+            console.log('fetchGenderStart error', e);
+        }
+    }
+
+}
+export const fetchGenderSuccsess = (genderData) => ({
+    type: actionTypes.FETCH_GENDER_SUCCESS,
+    data: genderData
+})
+export const fetchGenderFailed = () => ({
+    type: actionTypes.FETCH_GENDER_FAILED
+})
+
