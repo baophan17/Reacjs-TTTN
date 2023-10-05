497f71040255dd8431958d3e374960ba54807bc7
baophan17 <baop99720@gmail.com>
2023-10-05 23:40:48 +0700

    Complete the interface add detailed doctor information

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -10,6 +10,7 @@ const initialState = {
     allDoctors: [],
     allScheduleTime: [],
 
+    allRequiredDoctorInfor: [],
 
 }
 
@@ -95,6 +96,17 @@ const adminReducer = (state = initialState, action) => {
             }
         case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
             state.allScheduleTime = [];
+
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_SUCCESS:
+            state.allRequiredDoctorInfor = action.data;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_FAILED:
+            state.allRequiredDoctorInfor = [];
 
             return {
                 ...state,
